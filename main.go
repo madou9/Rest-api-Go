8b27a097da3c5e5bc28468616099a243d7d2e524
madou9 <issoufou.mamadou@gmail.com>
2025-05-02 18:12:06 +0200

    added POST api

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 // The struct tags (json:"...") are used for JSON serialization.
 type todo struct {
 	ID			string `json:"id"`
-	Item		string `json:"title"`
+	Item		string `json:"item"`
 	Completed	bool	`json:"completed"`
 }
 
@@ -25,8 +25,20 @@ func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos) // Indented JSON makes the response easier to read in tools like Postman or browsers.
 }
 
+// POST a new todo
+func addTodo(c *gin.Context) {
+	var newTodo todo
+	if err := c.BindJSON(&newTodo); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "invalid JSON"})
+		return
+	}
+	todos = append(todos, newTodo)
+	c.IndentedJSON(http.StatusCreated, newTodo)
+}
+
 func main() {
 	router := gin.Default() // Create a default Gin router with logging and recovery middleware.
 	router.GET("/todos", getTodos) // Define a GET route for /todos, and attach the getTodos handler.
+	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
